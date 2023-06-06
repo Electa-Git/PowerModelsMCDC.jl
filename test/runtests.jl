b924bf99ea6f089ece411c57bd637a4e4dd4d4b4
Matteo Rossini <matteo.rossini@rse-web.it>
2023-06-06 14:35:28 +0200

    Reduce console output during unit tests
    - Suppress info and warning messages from PowerModels and PowerModelsMCDC.
    - Suppress Ipopt banner.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,19 @@
 import PowerModels as _PM
 import PowerModelsMCDC as _PMMCDC
 import Ipopt
+import Memento
 using Test
+
+
+# Suppress warnings during testing.
+Memento.setlevel!(Memento.getlogger(_PM), "error")
+Memento.setlevel!(Memento.getlogger(_PMMCDC), "error")
 
 const _PMMCDC_dir = dirname(dirname(pathof(_PMMCDC))) # Root directory of PowerModelsMCDC package
 
-nlp_optimizer = _PMMCDC.optimizer_with_attributes(Ipopt.Optimizer, "tol" => 1e-6, "print_level" => 0)
+nlp_optimizer = _PMMCDC.optimizer_with_attributes(
+    Ipopt.Optimizer, "tol" => 1e-6, "print_level" => 0, "sb" => "yes"
+)
 
 
 @testset "PowerModelsMCDC" begin
