bb44cd7088e1ace35fa9915ad91b61359dce49b2
Matteo Rossini <matteo.rossini@kuleuven.be>
2024-07-31 16:58:09 +0200

    Simplify `add_ref_dcgrid!`
    The previous if/else structure was challenging to maintain. Any new dictionary had to be added in two places, and changes needed to be coordinated in both branches.
    With this approach, we create dictionaries for the main DC components at the beginning of the function, in case they don't already exist. This eliminates the need to separately consider the case where those dictionaries don't exist.

--- src/core/base.jl
+++ src/core/base.jl
@@ -8,146 +8,129 @@ function add_ref_dcgrid!(ref::Dict{Symbol,<:Any}, data::Dict{String,<:Any})
         delete!(nw_ref, :arcs_to_dc)
         delete!(nw_ref, :arcs_dc)
 
+        # Add dictionaries for DC components if they don't already exist
         if !haskey(nw_ref, :busdc)
             nw_ref[:busdc] = Dict{Int,Any}()
         end
-
         if !haskey(nw_ref, :branchdc)
-            # Components
             nw_ref[:branchdc] = Dict{Int,Any}()
-            # DC arcs
-            nw_ref[:arcsdc_from] = Vector{Tuple{Int,Int,Int}}()
-            nw_ref[:arcsdc_to]   = Vector{Tuple{Int,Int,Int}}()
-            nw_ref[:arcsdc]      = Vector{Tuple{Int,Int,Int}}()
-            # Lookups
-            nw_ref[:arcdc_conductors] = Dict{Tuple{Int,Int,Int},Any}()
-            nw_ref[:busdc_terminal_arcdc_conductors] = Dict([((bus["busdc_i"], c), Dict()) for c in 1:3 for (i, bus) in nw_ref[:busdc]])
-        else
-            # Filter DC branches that are inactive or connected to nonexistent buses
-            nw_ref[:branchdc] = Dict([x for x in nw_ref[:branchdc] if (
-                any(values(x.second["status"]) .== 1) &&
-                x.second["fbusdc"] in keys(nw_ref[:busdc]) &&
-                x.second["tbusdc"] in keys(nw_ref[:busdc])
-            )])
-
-            # DC arcs: tuples of the form (l,i,j) where l is the DC branch and i and j are the adjacent DC buses
-            nw_ref[:arcsdc_from] = [(l, branch["fbusdc"], branch["tbusdc"]) for (l, branch) in nw_ref[:branchdc]]
-            nw_ref[:arcsdc_to]   = [(l, branch["tbusdc"], branch["fbusdc"]) for (l, branch) in nw_ref[:branchdc]]
-            nw_ref[:arcsdc]      = [nw_ref[:arcsdc_from]; nw_ref[:arcsdc_to]]
-
-            # Map DC arcs to their active conductors
-            arcdc_conductors = Dict((l, i, j) => (Vector{Int}(), nw_ref[:branchdc][l]["conductors"]) for (l, i, j) in nw_ref[:arcsdc])
-            nw_ref[:arcdc_conductors] = arcdc_conductors # Will be populated later
-
-            # Map DC bus terminals to connected DC arc conductors
-            busdc_terminal_arcdc_conductors = Dict([((bus["busdc_i"], c), Dict()) for c in 1:3 for (i, bus) in nw_ref[:busdc]])
-            for (l, i, j) in nw_ref[:arcsdc]
-                if nw_ref[:branchdc][l]["conductors"] == 2
-                    terminals = _component_busdc_terminal_lookup[nw_ref[:branchdc][l]["connect_at"]]
-                else
-                    terminals = 1:3
-                end
-                for (c, terminal) in enumerate(terminals)
-                    if !iszero(nw_ref[:branchdc][l]["status"][c])
-                        push!(first(arcdc_conductors[(l, i, j)]), c)
-                        push!(busdc_terminal_arcdc_conductors[(i, terminal)], (l, i, j) => c)
-                    end
-                end
-            end
-            nw_ref[:busdc_terminal_arcdc_conductors] = busdc_terminal_arcdc_conductors
+        end
+        if !haskey(nw_ref, :convdc)
+            nw_ref[:convdc] = Dict{Int,Any}()
         end
 
-        if !haskey(nw_ref, :convdc)
-            # Components
-            nw_ref[:convdc] = Dict{Int,Any}()
-            # Lookups
-            nw_ref[:bus_convs] = Dict([(i, []) for (i, bus) in nw_ref[:bus]])
-            nw_ref[:busdc_terminal_conv_poles] = Dict{Tuple{Int,Int},Any}()
-            nw_ref[:conv_acpoles] = Dict{Int,Any}()
-            nw_ref[:conv_dcpoles] = Dict{Int,Any}()
-            nw_ref[:busdc_grounded_convs] = Dict{Tuple{Int,Int},Any}()
-            nw_ref[:ref_buses_dc] = Dict{Int,Any}()
-        else
-            # Filter converters that are inactive or connected to nonexistent buses
-            nw_ref[:convdc] = Dict([x for x in nw_ref[:convdc] if (
-                any(values(x.second["status"]) .== 1) &&
-                x.second["busdc_i"] in keys(nw_ref[:busdc]) &&
-                x.second["busac_i"] in keys(nw_ref[:bus])
-            )])
+        # Filter DC branches that are inactive or connected to nonexistent buses
+        nw_ref[:branchdc] = Dict([x for x in nw_ref[:branchdc] if (
+            any(values(x.second["status"]) .== 1) &&
+            x.second["fbusdc"] in keys(nw_ref[:busdc]) &&
+            x.second["tbusdc"] in keys(nw_ref[:busdc])
+        )])
 
-            # Map AC bus to connected converters
-            bus_convs = Dict([(i, []) for (i, bus) in nw_ref[:bus]])
-            for (i, conv) in nw_ref[:convdc]
-                push!(bus_convs[conv["busac_i"]], i)
+        # DC arcs: tuples of the form (l,i,j) where l is the DC branch and i and j are the adjacent DC buses
+        nw_ref[:arcsdc_from] = [(l, branch["fbusdc"], branch["tbusdc"]) for (l, branch) in nw_ref[:branchdc]]
+        nw_ref[:arcsdc_to]   = [(l, branch["tbusdc"], branch["fbusdc"]) for (l, branch) in nw_ref[:branchdc]]
+        nw_ref[:arcsdc]      = [nw_ref[:arcsdc_from]; nw_ref[:arcsdc_to]]
+
+        # Map DC arcs to their active conductors
+        arcdc_conductors = Dict((l, i, j) => (Vector{Int}(), nw_ref[:branchdc][l]["conductors"]) for (l, i, j) in nw_ref[:arcsdc])
+        nw_ref[:arcdc_conductors] = arcdc_conductors # Will be populated later
+
+        # Map DC bus terminals to connected DC arc conductors
+        busdc_terminal_arcdc_conductors = Dict([((bus["busdc_i"], c), Dict()) for c in 1:3 for (i, bus) in nw_ref[:busdc]])
+        for (l, i, j) in nw_ref[:arcsdc]
+            if nw_ref[:branchdc][l]["conductors"] == 2
+                terminals = _component_busdc_terminal_lookup[nw_ref[:branchdc][l]["connect_at"]]
+            else
+                terminals = 1:3
             end
-            nw_ref[:bus_convs] = bus_convs
-
-            # Map converter to active poles on the AC side
-            conv_acpoles = Dict(i => (findall(x -> !iszero(x), conv["status"]), conv["poles"]) for (i, conv) in nw_ref[:convdc])
-            nw_ref[:conv_acpoles] = conv_acpoles # Will be populated later
-
-            # Map converter to active poles on the DC side
-            conv_dcpoles = Dict(i => (Vector{Int}(), conv["poles"]+1) for (i, conv) in nw_ref[:convdc])
-            nw_ref[:conv_dcpoles] = conv_dcpoles # Will be populated later
-
-            # Map DC bus terminal to connected converter poles
-            busdc_terminal_conv_poles = Dict([((bus["busdc_i"], c), Dict()) for c in 1:3 for (i, bus) in nw_ref[:busdc]])
-            for (i, conv) in nw_ref[:convdc]
-                bus = conv["busdc_i"]
-                status_cond = findall(x->iszero(x), conv["status"])
-                if conv["poles"] == 1
-                    terminals = _component_busdc_terminal_lookup[conv["connect_at"]]
-                else
-                    terminals = 1:3
-                end
-                for (c, terminal) in enumerate(terminals)
-                    if !(c in status_cond)
-                        push!(first(conv_dcpoles[i]), c)
-                        push!(busdc_terminal_conv_poles[(bus, terminal)], i => c)
-                    end
+            for (c, terminal) in enumerate(terminals)
+                if !iszero(nw_ref[:branchdc][l]["status"][c])
+                    push!(first(arcdc_conductors[(l, i, j)]), c)
+                    push!(busdc_terminal_arcdc_conductors[(i, terminal)], (l, i, j) => c)
                 end
             end
-            nw_ref[:busdc_terminal_conv_poles] = busdc_terminal_conv_poles
+        end
+        nw_ref[:busdc_terminal_arcdc_conductors] = busdc_terminal_arcdc_conductors
 
-            # Map DC bus to connected grounded converters
-            busdc_grounded_convs = Dict([((bus["busdc_i"], c), Int[]) for c in 1:3 for (i, bus) in nw_ref[:busdc]])
-            for (i, conv) in nw_ref[:convdc]
-                bus = conv["busdc_i"]
-                if conv["ground_type"] == 1
-                    push!(busdc_grounded_convs[(bus, 3)], i) # (bus, 3) for selecting 3rd terminal of the relevant dc bus whereas i is for selecting the variable
+        # Filter converters that are inactive or connected to nonexistent buses
+        nw_ref[:convdc] = Dict([x for x in nw_ref[:convdc] if (
+            any(values(x.second["status"]) .== 1) &&
+            x.second["busdc_i"] in keys(nw_ref[:busdc]) &&
+            x.second["busac_i"] in keys(nw_ref[:bus])
+        )])
+
+        # Map AC bus to connected converters
+        bus_convs = Dict([(i, []) for (i, bus) in nw_ref[:bus]])
+        for (i, conv) in nw_ref[:convdc]
+            push!(bus_convs[conv["busac_i"]], i)
+        end
+        nw_ref[:bus_convs] = bus_convs
+
+        # Map converter to active poles on the AC side
+        conv_acpoles = Dict(i => (findall(x -> !iszero(x), conv["status"]), conv["poles"]) for (i, conv) in nw_ref[:convdc])
+        nw_ref[:conv_acpoles] = conv_acpoles # Will be populated later
+
+        # Map converter to active poles on the DC side
+        conv_dcpoles = Dict(i => (Vector{Int}(), conv["poles"]+1) for (i, conv) in nw_ref[:convdc])
+        nw_ref[:conv_dcpoles] = conv_dcpoles # Will be populated later
+
+        # Map DC bus terminal to connected converter poles
+        busdc_terminal_conv_poles = Dict([((bus["busdc_i"], c), Dict()) for c in 1:3 for (i, bus) in nw_ref[:busdc]])
+        for (i, conv) in nw_ref[:convdc]
+            bus = conv["busdc_i"]
+            status_cond = findall(x->iszero(x), conv["status"])
+            if conv["poles"] == 1
+                terminals = _component_busdc_terminal_lookup[conv["connect_at"]]
+            else
+                terminals = 1:3
+            end
+            for (c, terminal) in enumerate(terminals)
+                if !(c in status_cond)
+                    push!(first(conv_dcpoles[i]), c)
+                    push!(busdc_terminal_conv_poles[(bus, terminal)], i => c)
                 end
             end
-            nw_ref[:busdc_grounded_convs] = busdc_grounded_convs
+        end
+        nw_ref[:busdc_terminal_conv_poles] = busdc_terminal_conv_poles
 
-            # DC reference buses
-            ref_buses_dc = Dict{Int,Any}()
+        # Map DC bus to connected grounded converters
+        busdc_grounded_convs = Dict([((bus["busdc_i"], c), Int[]) for c in 1:3 for (i, bus) in nw_ref[:busdc]])
+        for (i, conv) in nw_ref[:convdc]
+            bus = conv["busdc_i"]
+            if conv["ground_type"] == 1
+                push!(busdc_grounded_convs[(bus, 3)], i) # (bus, 3) for selecting 3rd terminal of the relevant dc bus whereas i is for selecting the variable
+            end
+        end
+        nw_ref[:busdc_grounded_convs] = busdc_grounded_convs
+
+        # DC reference buses
+        ref_buses_dc = Dict{Int,Any}()
+        for (k, v) in nw_ref[:convdc]
+            if v["type_dc"] == 2
+                ref_buses_dc[k] = v
+            end
+        end
+        if length(ref_buses_dc) == 0
             for (k, v) in nw_ref[:convdc]
-                if v["type_dc"] == 2
+                if v["type_ac"] == 2
                     ref_buses_dc[k] = v
                 end
             end
-            if length(ref_buses_dc) == 0
-                for (k, v) in nw_ref[:convdc]
-                    if v["type_ac"] == 2
-                        ref_buses_dc[k] = v
-                    end
-                end
-                Memento.warn(_PM._LOGGER, "no reference DC bus found, setting reference bus based on AC bus type")
+            Memento.warn(_PM._LOGGER, "no reference DC bus found, setting reference bus based on AC bus type")
+        end
+        if length(ref_buses_dc) > 1
+            ref_buses_warn = ""
+            for (rb) in keys(ref_buses_dc)
+                ref_buses_warn = ref_buses_warn * "$rb, "
             end
-            if length(ref_buses_dc) > 1
-                ref_buses_warn = ""
-                for (rb) in keys(ref_buses_dc)
-                    ref_buses_warn = ref_buses_warn * "$rb, "
-                end
-                Memento.warn(_PM._LOGGER, "multiple reference buses found, i.e. " * ref_buses_warn * "this can cause infeasibility if they are in the same connected component")
-            end
-            nw_ref[:ref_buses_dc] = ref_buses_dc
+            Memento.warn(_PM._LOGGER, "multiple reference buses found, i.e. " * ref_buses_warn * "this can cause infeasibility if they are in the same connected component")
+        end
+        nw_ref[:ref_buses_dc] = ref_buses_dc
 
-            # Warn if there are converters with power fixed on both sides
-            for (c, conv) in nw_ref[:convdc]
-                if conv["type_dc"] == 1 && conv["type_ac"] in (1,2)
-                    Memento.warn(_PM._LOGGER, "For converter $c is chosen P is fixed on AC and DC side. This can lead to infeasibility in the PF problem.")
-                end
+        # Warn if there are converters with power fixed on both sides
+        for (c, conv) in nw_ref[:convdc]
+            if conv["type_dc"] == 1 && conv["type_ac"] in (1,2)
+                Memento.warn(_PM._LOGGER, "For converter $c is chosen P is fixed on AC and DC side. This can lead to infeasibility in the PF problem.")
             end
         end
     end
